837a3bc217d6df48f3670b5165fe46659cd9b0d6
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-06-27 17:49:50 +0200

    Create project models

---
+++ BankAccountAPI/Models/Account.cs
@@ -0,0 +1,12 @@
+﻿namespace BankAccountAPI.Models
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public string BankName { get; set; }
+        public string AccountNumber { get; set; }
+        public string IBAN { get; set; }
+        public string Currency { get; set; }
+        public User User { get; set; }
+    }
+}
