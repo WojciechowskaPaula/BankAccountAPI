33a41f689b242cdfb7f8c78a1200f44dde6380f7
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-06-27 23:47:07 +0200

    Create GetAllUsers method

---
+++ BankAccountAPI/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using BankAccountAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankAccountAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public UserController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet]
+        [Route("GetAllUsers")]
+        public IActionResult GetAllUsers()
+        {
+            var usersList = _applicationDbContext.Users.ToList();
+            return Ok(usersList);
+        }
+    }
+}
