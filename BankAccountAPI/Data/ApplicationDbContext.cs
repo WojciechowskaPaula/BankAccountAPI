46fbb05d62ba0da9e61bcc251396f4ed71537062
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-06-27 18:05:08 +0200

    Creating dbContext

---
+++ BankAccountAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BankAccountAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankAccountAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+    } 
+}
+
+    
+    
+
