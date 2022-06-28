5741090b125fbfcfe1e447d0261bc5cee2e696b1
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-06-28 12:57:14 +0200

    Adding models validation

--- BankAccountAPI/Models/Account.cs
+++ BankAccountAPI/Models/Account.cs
@@ -1,11 +1,31 @@
-﻿namespace BankAccountAPI.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankAccountAPI.Models
 {
     public class Account
     {
+        [Required]
+        [DisplayName("Account ID")]
         public int AccountId { get; set; }
+
+        [Required]
+        [DisplayName("Bank name")]
+        [StringLength(30, MinimumLength =3, ErrorMessage = "Bank name cannot be longer than 30 characters and less than 2 characters")]
         public string BankName { get; set; }
+
+        [Required]
+        [DisplayName("Account number")]
+        [StringLength(20, MinimumLength =7, ErrorMessage ="Account number is not valid")]
+        [RegularExpression("^[^-][0 - 9]$", ErrorMessage ="The Account number must contain only numbers")]
         public string AccountNumber { get; set; }
+
+        [Required]
+        [MinLength(8)]
         public string IBAN { get; set; }
+
+        [Required]
+        [MaxLength(30)]
         public string Currency { get; set; }
         public User User { get; set; }
     }
--- BankAccountAPI/Models/User.cs
+++ BankAccountAPI/Models/User.cs
@@ -1,12 +1,30 @@
-﻿namespace BankAccountAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankAccountAPI.Models
 {
     public class User
     {
+        [Required]
         public int UserID { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "First name cannot be longer than 30 characters and less than 2 characters")]
         public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "First name cannot be longer than 30 characters and less than 2 characters")]
         public string LastName { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\s*\+?\s*([0-9][\s-]*){9,}$", ErrorMessage="Not valid Phone number")]
         public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [MinLength(3)]
         public string Nationality { get; set; }
 
     }
