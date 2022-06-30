aedb479437dee2e955fa1b315359b0f7aedf88b1
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-06-30 16:00:58 +0200

    Adding try/catch to UserController

--- BankAccountAPI/Controllers/UserController.cs
+++ BankAccountAPI/Controllers/UserController.cs
@@ -22,83 +22,132 @@ namespace BankAccountAPI.Controllers
         [Route("GetAllUsers")]
         public IActionResult GetAllUsers()
         {
-            _logger.LogInformation("action=getAllUsers");
-            var usersList = _applicationDbContext.Users.ToList();
-            _logger.LogInformation($"action=getAllUsers usersCount:{usersList.Count}");
-            return Ok(usersList);
+            try
+            {
+                _logger.LogInformation("action=getAllUsers");
+                var usersList = _applicationDbContext.Users.ToList();
+                _logger.LogInformation($"action=getAllUsers usersCount:{usersList.Count}");
+                return Ok(usersList);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"action=getAllUsers msg='{ex.Message}'", ex);
+                return StatusCode(500);
+            }
         }
 
         [HttpGet]
         [Route("GetUser")]
         public IActionResult GetUser(int id)
         {
-            _logger.LogInformation($"action=getUser id={id}");
-            var user = _applicationDbContext.Users.Where(x => x.UserID == id).FirstOrDefault();
-            if (user == null)
+            try
             {
-                _logger.LogWarning($"action=getUser msg='User with {id} not found'");
-                return NotFound($"User with {id} not found");
+                _logger.LogInformation($"action=getUser id={id}");
+                var user = _applicationDbContext.Users.Where(x => x.UserID == id).FirstOrDefault();
+                if (user == null)
+                {
+                    _logger.LogWarning($"action=getUser msg='User with {id} not found'");
+                    return NotFound($"User with {id} not found");
+                }
+                _logger.LogInformation($"action=getUser msg='user with {id} was found'");
+                return Ok(user);
             }
-            _logger.LogInformation($"action=getUser msg='user with {id} was found'");
-            return Ok(user);
+            catch (Exception ex)
+            {
+                _logger.LogError($"action=getUser msg='{ex.Message}'", ex);
+                return StatusCode(500);
+            }
         }
 
         [HttpPut]
         [Route("AddUser")]
         public IActionResult AddUser(User user)
         {
-            _logger.LogInformation($"action=addUser user='{JsonSerializer.Serialize(user)}'");
-            _applicationDbContext.Users.Add(user);
-            _applicationDbContext.SaveChanges();
-            _logger.LogInformation("action=addUser msg='A new user has been saved'");
-            return Ok();
+            try
+            {
+                _logger.LogInformation($"action=addUser user='{JsonSerializer.Serialize(user)}'");
+                _applicationDbContext.Users.Add(user);
+                _applicationDbContext.SaveChanges();
+                _logger.LogInformation("action=addUser msg='A new user has been saved'");
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"action=addUser msg='{ex.Message}'", ex);
+                return StatusCode(500);
+            }
         }
 
         [HttpDelete]
         [Route("DeleteUser")]
         public IActionResult DeleteUser(int id)
         {
-            _logger.LogInformation($"action=deleteUser id={id}");
-            var user = _applicationDbContext.Users.Where(x => x.UserID == id).FirstOrDefault();
-            if (user == null)
+            try
             {
-                _logger.LogWarning($"action=getUser msg='User with {id} not found'");
-                return NotFound("User not found");
+                _logger.LogInformation($"action=deleteUser id={id}");
+                var user = _applicationDbContext.Users.Where(x => x.UserID == id).FirstOrDefault();
+                if (user == null)
+                {
+                    _logger.LogWarning($"action=getUser msg='User with {id} not found'");
+                    return NotFound("User not found");
+                }
+                _applicationDbContext.Users.Remove(user);
+                _applicationDbContext.SaveChanges();
+                _logger.LogInformation($"action=deleteUser msg='User with {id} has been removed'");
+                return Ok(user);
             }
-            _applicationDbContext.Users.Remove(user);
-            _applicationDbContext.SaveChanges();
-            _logger.LogInformation($"action=deleteUser msg='User with {id} has been removed'");
-            return Ok(user);
+            catch (Exception ex)
+            {
+                _logger.LogError($"action=deleteUser msg='{ex.Message}'", ex);
+                return StatusCode(500);
+            }
         }
 
         [HttpGet]
         [Route("GetUsersByNationality")]
         public IActionResult GetUsersByNationality(string nationality)
         {
-            _logger.LogInformation($"action=getUsersByNationality nationality={nationality}");
-            var users = _applicationDbContext.Users.Where(x => x.Nationality == nationality).ToList();
-            if (users.Count > 0)
+            try
             {
-                _logger.LogInformation($"action=getUsersByNationality usersCount={users.Count}");
-                return Ok(users);
+                _logger.LogInformation($"action=getUsersByNationality nationality={nationality}");
+                var users = _applicationDbContext.Users.Where(x => x.Nationality == nationality).ToList();
+                if (users.Count > 0)
+                {
+                    _logger.LogInformation($"action=getUsersByNationality usersCount={users.Count}");
+                    return Ok(users);
+                }
+                _logger.LogWarning($"action=getUsersByNationality msg='Users with {nationality} not found'");
+                return NotFound($"Users with {nationality} not found");
             }
-            _logger.LogWarning($"action=getUsersByNationality msg='Users with {nationality} not found'");
-            return NotFound($"Users with {nationality} not found");
+            catch (Exception ex)
+            {
+                _logger.LogError($"action=getUsersByNationality msg='{ex.Message}'", ex);
+                return StatusCode(500);
+            }
         }
 
         [HttpGet]
         [Route("GetUserByPhoneNumber")]
         public IActionResult GetUserByPhoneNumber(string phoneNumber)
         {
-            _logger.LogInformation("action=getUserByPhoneNumber");
-            var user = _applicationDbContext.Users.Where(x => x.PhoneNumber == phoneNumber).FirstOrDefault();
-            if (user != null)
+            try
             {
-                _logger.LogInformation($"action=getUserByPhoneNumber msg='User with phone number: {phoneNumber} was found'");
-                return Ok(user);
+                _logger.LogInformation("action=getUserByPhoneNumber");
+                var user = _applicationDbContext.Users.Where(x => x.PhoneNumber == phoneNumber).FirstOrDefault();
+                if (user != null)
+                {
+                    _logger.LogInformation($"action=getUserByPhoneNumber msg='User with phone number: {phoneNumber} was found'");
+                    return Ok(user);
+                }
+                _logger.LogWarning($"action=getUserByPhoneNumber msg='User with {phoneNumber} not found'");
+                return NotFound($"User with {phoneNumber} not found");
             }
-            _logger.LogWarning($"action=getUserByPhoneNumber msg='User with {phoneNumber} not found'");
-            return NotFound($"User with {phoneNumber} not found");
+
+            catch (Exception ex)
+            {
+                _logger.LogError($"action = getUserByPhoneNumber msg='{ex.Message}'", ex);
+                return StatusCode(500);
+            }
         }
     }
 }
